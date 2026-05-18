568353fa9ff415eaf1ca2c84c544fd2a14d6662b
pauulaferreiro <159444174+pauulaferreiro@users.noreply.github.com>
2026-05-18 14:09:43 +0200

    update token

--- pipeline_docker.py
+++ pipeline_docker.py
@@ -13,7 +13,7 @@ from transformers import AutoModelForImageTextToText, AutoProcessor, BitsAndByte
 from logger_config import log, monitor_latency, latency_log
 from metrics_monitor import ResourceMonitor, aggregate_metric_dicts
 
-os.environ["HF_TOKEN"] = "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" #token READ
+os.environ["HF_TOKEN"] = "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"
 
 #MODEL_ID = "Qwen/Qwen2.5-VL-3B-Instruct"
 #MODEL_ID = "mistralai/Ministral-3-3B-Instruct-2512-BF16"
